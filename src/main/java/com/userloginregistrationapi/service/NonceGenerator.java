2a055f02de5403aa4705c1cc70494f64c496f1fa
Ahmed Shalabi <shtk3eb01@gmail.com>
2024-03-17 00:22:18 +0200

    Create nonce generator class to generate the server nonce

---
+++ src/main/java/com/userloginregistrationapi/service/NonceGenerator.java
@@ -0,0 +1,18 @@
+package com.userloginregistrationapi.service;
+
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.UUID;
+
+@Service
+public class NonceGenerator {
+
+    public String generateNonceWithDateSeed() {
+        LocalDate currentDate = LocalDate.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        String seed = formatter.format(currentDate);
+        return UUID.nameUUIDFromBytes(seed.getBytes()).toString();
+    }
+}
