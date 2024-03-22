bf60be5ed09c19b56e052d048184f4a7c702e209
Ahmed Shalabi <shtk3eb01@gmail.com>
2024-03-22 23:18:44 +0200

    Add the validation for the registration model and login model.

--- src/main/java/com/userloginregistrationapi/model/UserLoginModel.java
+++ src/main/java/com/userloginregistrationapi/model/UserLoginModel.java
@@ -1,5 +1,8 @@
 package com.userloginregistrationapi.model;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,6 +12,11 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class UserLoginModel {
 
+    @NotBlank(message = "ID cannot be blank")
     private String id;
+    @NotBlank(message = "Password cannot be empty")
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[A-Z])(?=.*[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>/?]).+$",
+            message = "Password must include at least one number one capital letter and one symbol")
+    @Size(min = 7, message = "Password must include at least 7 characters")
     private String password;
 }
