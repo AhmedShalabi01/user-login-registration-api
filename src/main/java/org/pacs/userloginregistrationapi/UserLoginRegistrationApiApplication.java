81bc17223c6aaca8b50ad19d8d77194e74b7e90e
AhmedShalabi01 <shtk3eb01@gmail.com>
2024-05-24 20:40:45 +0300

    Code refactor

--- src/main/java/org/pacs/userloginregistrationapi/UserLoginRegistrationApiApplication.java
+++ src/main/java/org/pacs/userloginregistrationapi/UserLoginRegistrationApiApplication.java
@@ -2,11 +2,10 @@ package org.pacs.userloginregistrationapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.reactive.function.client.WebClient;
 
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@SpringBootApplication
 public class UserLoginRegistrationApiApplication {
 
 	@Bean
